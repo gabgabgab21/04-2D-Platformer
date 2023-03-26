7509cd57899c959f1e3e1fb917b5ecd973e092d9
gabgabgab21 <122471903+gabgabgab21@users.noreply.github.com>
2023-03-26 14:32:38 -0400

    completes excercise

--- Enemy/Enemy_Ground.gd
+++ Enemy/Enemy_Ground.gd
@@ -1,6 +1,23 @@
 extends KinematicBody2D
 
-export var speed = 100
+export var speed = 2
+export var damage = 10
+var direction = 1
+var velocity = Vector2.ZERO
 
-func _ready():
-	pass
+func _physics_process(_delta):
+	if direction < 0 and !$Sprite.flip_h:
+		$Sprite.flip_h = true
+	if direction > 0 and $Sprite.flip_h:
+		$Sprite.flip_h = false
+	velocity.x += direction * speed
+	move_and_slide(velocity, Vector2.UP)
+	
+		
+
+func _on_Area2D_body_entered(body):
+	if velocity.x != 0 and (body.name == "Platform" or body.name == "Ground"):
+		velocity.x = 0
+		direction *= -1
+	if body.name == "Player":
+		body.do_damage(damage)
